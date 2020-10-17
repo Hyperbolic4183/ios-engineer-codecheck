53315297cec202d62d0526d36c0e7499df9a8b60
otsuka.amane <otsuka.amane@gmail.com>
2020-10-17 12:02:43 +0900

    RealmSwiftをインストールした。

---
+++ iOSEngineerCodeCheck/Controllers/FavoriteRipositoriesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FavoriteRipositoriesViewController.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by 大塚周 on 2020/10/16.
+//  Copyright © 2020 YUMEMI Inc. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteRipositoriesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ iOSEngineerCodeCheck/Controllers/FavoritedRepositories.swift
@@ -0,0 +1,9 @@
+//
+//  FavoritedRepositories.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by 大塚周 on 2020/10/16.
+//  Copyright © 2020 YUMEMI Inc. All rights reserved.
+//
+
+import Foundation
